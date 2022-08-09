99ff9fe867d8a68baf23987478c4494c84c83f71
Geo3490 <geo40127@gmail.com>
2022-08-09 17:09:54 -0400

    Initial commit

---
+++ src/com/javaLearn/Account.java
@@ -0,0 +1,31 @@
+package com.javaLearn;
+
+public class Account {
+    int balance;
+    int previousTransaction;
+
+    String customerName;
+    String customerId;
+
+
+    void Account(String cName, String cId){
+
+        customerName = cName;
+        customerId = cId;
+
+    }
+
+    void deposit(int amount){
+
+    }
+
+    void withdraw(int amount){
+
+    }
+
+    void getPreviousTransaction(){
+
+    }
+
+
+}
